10003c2aef11f17686ff9048cef3d4a833eaecb1
MrL21-Andreani <80051449+MrL21-Andreani@users.noreply.github.com>
2021-06-29 15:11:19 -0300

    Especifico puerto en la app

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:3.8
 WORKDIR /test_docker
 
 ENV FLASK_APP app.py
-ENV FLASK_RUN_HOST 0.0.0.0
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", debug=True, port=8080)
 
 
 
